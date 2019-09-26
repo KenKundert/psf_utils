02b7c0fc4cdf2721f6ab1826340ee6f328172aa4
Ken Kundert <ken@theKunderts.net>
2019-09-25 19:11:16 -0700

    bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='psf_utils',
-    version='0.0.3',
+    version='0.1.0',
     description='Cadence PSF file utilities',
     long_description=readme,
     author="Ken Kundert",
