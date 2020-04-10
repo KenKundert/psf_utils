a78cd4627b0b8bd81e28fc0278e4aa1966b2ca95
Ken Kundert <ken@theKunderts.net>
2020-04-09 18:21:06 -0700

    Nits.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     url = 'https://psf_utils.readthedocs.io',
     download_url = 'https://github.com/kenkundert/psf_utils/tarball/master',
     license = 'GPLv3+',
-    zip_safe = True,
+    zip_safe = False,
     packages = 'psf_utils'.split(),
     entry_points = {'console_scripts': [
         'list-psf = psf_utils.list:list_signals',
