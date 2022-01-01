f0427bf07d6eda217fe7923f203d26553a0f0792
Ken Kundert <ken@theKunderts.net>
2022-01-01 10:18:53 -0800

    update copyright date

--- psf_utils/list.py
+++ psf_utils/list.py
@@ -18,6 +18,23 @@ then pickles the data and writes it to disk. On subsequent runs the pickled data
 is used if the pickle file is newer that the corresponding PSF file.
 """
 
+# License {{{1
+# Copyright (C) 2018-2022 Kenneth S. Kundert
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see [http://www.gnu.org/licenses/].
+
+
 # Imports {{{1
 from .show import expand_args, get_psf_filename
 from .psf import PSF
--- psf_utils/psf.py
+++ psf_utils/psf.py
@@ -2,6 +2,23 @@
 Read PSF File
 """
 
+# License {{{1
+# Copyright (C) 2018-2022 Kenneth S. Kundert
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see [http://www.gnu.org/licenses/].
+
+
 # Imports {{{1
 from .parse import ParsePSF, ParseError
 from inform import Error, Info, join, log, os_error
